e36fd072ca994c4922a5beb55cf6ae96cceb9125
mansour.dione <soura934@gmail.com>
2023-08-23 14:52:26 -0400

    implemented ui crud funtionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Employee } from './model/employee';
 import { EmployeeService } from './service/employee.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,8 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   public employees: Employee[] = [];
+  public editEmployee?: Employee;
+  public deleteEmployee?: Employee;
 
   constructor(private employeeService: EmployeeService) {}
 
@@ -18,15 +21,14 @@ export class AppComponent implements OnInit {
   }
 
   getEmployees(): void {
-    this.employeeService.getAllEmployees().subscribe(
-      (next: Employee[]) => {
-        this.employees = next;
-        console.log(this.employees);
+    this.employeeService.getAllEmployees().subscribe({
+      next: (res: Employee[]) => {
+        this.employees = res;
       },
-      (error: HttpErrorResponse) => {
+      error: (error: HttpErrorResponse) => {
         alert(error.message);
-      }
-    );
+      },
+    });
   }
 
   public onModalOpen(status: string, employee?: Employee): void {
@@ -39,12 +41,50 @@ export class AppComponent implements OnInit {
       button.setAttribute('data-target', '#addEmployeeModal');
     }
     if (status === 'edit') {
+      this.editEmployee = employee;
       button.setAttribute('data-target', '#updateEmployeeModal');
     }
     if (status === 'delete') {
+      this.deleteEmployee = employee;
       button.setAttribute('data-target', '#deleteEmployeeModal');
     }
     container?.appendChild(button);
     button.click();
+  }
+
+  onAddEmployee(addForm: NgForm): void {
+    document.getElementById('add-employee-form')?.click();
+    this.employeeService.addEmployee(addForm.value).subscribe({
+      next: (response: Employee) => {
+        this.getEmployees();
+        addForm.reset();
+      },
+      error: (err: HttpErrorResponse) => {
+        alert(err.message);
+      },
+    });
+  }
+
+  onEditEmployee(employee: Employee): void {
+    this.employeeService.editEmployee(employee).subscribe({
+      next: (response: Employee) => {
+        this.getEmployees();
+      },
+      error: (err: HttpErrorResponse) => {
+        alert(err.message);
+      },
+    });
+  }
+
+  onDeleteEmployee(employeeId: number): void {
+    console.log(employeeId);
+    this.employeeService.removeEmployee(employeeId).subscribe({
+      next: (response: void) => {
+        this.getEmployees();
+      },
+      error: (err: HttpErrorResponse) => {
+        alert(err.message);
+      },
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CommonModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
